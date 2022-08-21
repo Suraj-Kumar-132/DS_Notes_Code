87967c20ad5c9ebcb819b107c7b594101a22ef7d
Suraj <shivamsuraj132@gmail.com>
2022-08-21 15:58:07 +0530

    init

---
+++ LinearSearch_Iterative.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+
+    vector<int>vec(num);
+    for(int i=0;i<num;i++){
+        cin>>vec[i];
+
+    }
+    int x, flag =0;
+    cin>>x;
+
+    for(int i =0;i<num;i++){
+        if(vec[i] == x)
+        {
+            cout<<"found"<<endl;
+            flag =1;
+             break;
+        }
+    }
+    if(!flag){
+        cout<<"Not found";
+    }
+    return 0;
+}
